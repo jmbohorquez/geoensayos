c647097f42c445a61531bf263f2c72426e5290ff
Julian Bohorquez <jm.bohorquez@gmail.com>
2025-05-19 19:05:30 +0200

    actualizacion slider home 2

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+// /** @type {import('next').NextConfig} */
+
+// const path = require( 'path' )
+
+// const nextConfig = {
+//     reactStrictMode: true,
+//     sassOptions: {
+//         includePaths: [ path.join( __dirname, 'styles' ) ],
+//     }
+// }
+
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    
+};
+
+export default nextConfig;
